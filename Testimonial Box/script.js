b35f8c780e7ffa2d032d14f4326fe011cfc8ebd3
salma-soussi <soussiselma@hotmail.com>
2022-02-03 19:28:34 +0100

    third commit

--- Testimonial Box/script.js
+++ Testimonial Box/script.js
@@ -11,35 +11,35 @@ const testimonials = [
       photo:
         'https://randomuser.me/api/portraits/women/40.jpg',
       text:
-        "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+        "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this girl. This girl is an amazing developer. She stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a thoroughly thought out design and do their best to put it in code. She goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
     },
     {
       name: 'June Cha',
       position: 'Software Engineer',
       photo: 'https://randomuser.me/api/portraits/women/44.jpg',
       text:
-        'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
+        'This girl is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire her, you will not be disappointed by the work delivered. She will go the extra mile to make sure that you are happy with your project. I will surely work again with her!',
     },
     {
       name: 'Iida Niskanen',
       position: 'Data Entry',
       photo: 'https://randomuser.me/api/portraits/women/68.jpg',
       text:
-        "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+        "This girl is a hard worker. Communication was also very good with her and she was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with her.",
     },
     {
       name: 'Renee Sims',
       position: 'Receptionist',
       photo: 'https://randomuser.me/api/portraits/women/65.jpg',
       text:
-        "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+        "This girl does everything she can to get the job done and done right. This is the second time I've hired her, and I'll hire her again in the future.",
     },
     {
       name: 'Jonathan Nunfiez',
       position: 'Graphic Designer',
       photo: 'https://randomuser.me/api/portraits/men/43.jpg',
       text:
-        "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
+        "I had my concerns that due to a tight deadline this project can't be done. But this girl proved me wrong not only she delivered an outstanding work but she managed to deliver 1 day prior to the deadline. And when I asked for some revisions she made them in MINUTES. I'm looking forward to work with her again and I totally recommend her. Thanks again!",
     },
     {
       name: 'Sasha Ho',
@@ -47,14 +47,14 @@ const testimonials = [
       photo:
         'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
       text:
-        'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
+        'This girl is a top notch designer and front end developer. she communicates well, works fast and produces quality work. We have been lucky to work with her!',
     },
     {
       name: 'Veeti Seppanen',
       position: 'Director',
       photo: 'https://randomuser.me/api/portraits/men/97.jpg',
       text:
-        'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
+        'This girl is a young and talented IT professional, proactive and responsible, with a strong work ethic. she is very strong in PSD2HTML conversions and HTML/CSS technology. She is a quick learner, eager to learn new technologies. She is focused and has the good dynamics to achieve due dates and outstanding results.',
     },
   ]
 
--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const nav = document.querySelector('.nav')
+const cards = document.querySelectorAll('.card');
+const headers = document.querySelectorAll('.header');
+const titles = document.querySelectorAll('.title');
+const profile_imgs = document.querySelectorAll('.profile-img');
+const nameAuthors = document.querySelectorAll('.name');
+const dates = document.querySelectorAll('.date');
+// Get all the elements with the class animated-bg or animated-bg-text
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+
+const DATES = ['Dec 30, 2021','Oct 11, 2021','Oct 20, 2021','Dec 20, 2021','Oct 15, 2021','Oct 21, 2021','Nov 06, 2021','Dec 24, 2021','Nov 15, 2021','Nov 23, 2021','Nov 28, 2021','Dec 13, 2021','Dec 09, 2021','Oct 03, 2021','Nov 10, 2021']
+const HEADERS = ['images/Checkboxes.png','images/Content-Placeholder.png','images/Dad-Jokes.png','images/Feedback-UI-Design.png','images/Hover-Draw-Board.png','images/Incrementing-Counter.png','images/Note-App.png','images/Password-Generator.png','images/Quiz-App.png','images/Random-Choice.png','images/Split-Landing-Page.png','images/Testimonial-Box.png','images/Theme-clock.png','images/Toast-notification.png','images/Todo-list.png']
+
+const TITLES = ['Checkboxes Design','Content Placeholder','Dad Jokes','Feedback UI Design','Hover-Draw Board','Incrementing Counter','Note App','Password  Generator','Quiz App','Random Choice Picker','Split Landing Page','Testimonial Box','Theme clock','Toast notification','Todo list']
+
+const Desc = ['Checkboxes','Content Placeholder','Dad Jokes','Feedback UI Design','Hover-Draw Board','Incrementing Counter','Note App','Password  Generator','Quiz App','Random Choice Picker','Split Landing Page','Testimonial Box','Theme clock','Toast notification','Todo list']
+
 window.addEventListener('scroll',fixNav)
 
 function fixNav() {
@@ -7,4 +25,34 @@ function fixNav() {
     }else{
         nav.classList.remove('active')
     }
+}
+
+
+setTimeout(getData, 2000);
+
+function getData() {
+    // Insert all the information that you want to show
+
+    headers.forEach((header,index) => {
+        header.innerHTML = `<img src="${HEADERS[index]}"
+    alt="" />`;
+    });
+    titles.forEach((title,index) => {
+        title.innerHTML = TITLES[index];
+    });
+    profile_imgs.forEach(profile_img => {
+        profile_img.innerHTML ='<img src="images/me.jpg" alt="" />';
+    });
+    nameAuthors.forEach(nameAuthor => {
+        nameAuthor.innerHTML = 'salma Soussi';
+    });
+
+    dates.forEach((date,index) => {
+        date.innerHTML = DATES[index];
+    });
+    
+    // Remouve the class animated-bg and animated-bg-text from all elements of the Objects
+    animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'));
+    animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'));
+   
 }
